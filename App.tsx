6e8e3f99027091e684e81294517a67b6b3de6aab
tkddl5990 <tkddl5990@gmail.com>
2025-01-05 16:21:26 +0900

    feat: react-navigation 의존 패키지 버전 수정

--- App.tsx
+++ App.tsx
@@ -1,42 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {Text, View} from 'react-native';
+import {createStaticNavigation} from '@react-navigation/native';
 
-function App() {
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function HomeScreen() {
   return (
-    <NavigationContainer>
-      <SafeAreaView style={styles.backgroundStyle}>
-        <View style={styles.container}>
-          <Text style={styles.title}>된건가?? pod-install 안되던데?</Text>
-        </View>
-      </SafeAreaView>
-    </NavigationContainer>
+    // <SafeAreaView
+    //   style={{
+    //     flex: 1,
+    //   }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'green',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+      <Text style={{color: '#fff'}}>Home 됬다리~~~</Text>
+    </View>
+    // </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  backgroundStyle: {
-    flex: 1,
-  },
-  container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 20,
-    color: '#fff',
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+function App() {
+  return (
+    // <SafeAreaProvider>
+    <Navigation />
+    // </SafeAreaProvider>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   backgroundStyle: {
+//     flex: 1,
+//   },
+//   container: {
+//     width: '100%',
+//     height: '100%',
+//     backgroundColor: 'blue',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontWeight: 500,
+//     fontSize: 20,
+//     color: '#fff',
+//   },
+// });
 
 export default App;
