63b2b70b4860bcd7f486d83a568c18320ad92193
tkddl5990 <tkddl5990@gmail.com>
2025-01-05 16:46:27 +0900

    feat: react-navigation sdtatic type 선언

--- App.tsx
+++ App.tsx
@@ -1,60 +1,30 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 import {createStaticNavigation} from '@react-navigation/native';
 
+import type {StaticParamList} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-function HomeScreen() {
-  return (
-    // <SafeAreaView
-    //   style={{
-    //     flex: 1,
-    //   }}>
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'green',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }}>
-      <Text style={{color: '#fff'}}>Home 됬다리~~~</Text>
-    </View>
-    // </SafeAreaView>
-  );
-}
+import HomePage from './src/pages/Home';
+import WritePage from './src/pages/Write';
 
 const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
   screens: {
-    Home: HomeScreen,
+    Home: HomePage,
+    Write: WritePage,
   },
 });
+
+type RootStackParamList = StaticParamList<typeof RootStack>;
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
 
 const Navigation = createStaticNavigation(RootStack);
 
 function App() {
-  return (
-    // <SafeAreaProvider>
-    <Navigation />
-    // </SafeAreaProvider>
-  );
+  return <Navigation />;
 }
-
-// const styles = StyleSheet.create({
-//   backgroundStyle: {
-//     flex: 1,
-//   },
-//   container: {
-//     width: '100%',
-//     height: '100%',
-//     backgroundColor: 'blue',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   title: {
-//     fontWeight: 500,
-//     fontSize: 20,
-//     color: '#fff',
-//   },
-// });
 
 export default App;
